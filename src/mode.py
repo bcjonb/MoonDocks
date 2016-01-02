af7a4232ec08605e08bd81cdfb0c003a795219d9
Brandon Crooks <crookrb@hotmail.com>
2016-01-02 09:04:02 -0500

    Add basic scructure

--- src/mode.py
+++ src/mode.py
@@ -5,23 +5,14 @@ import cmd
 
 class MoonDocks(cmd.Cmd):
   """MoonDocks"""
-  state = 0
-  prompt = 'Type help  or start to get started> '
 
-  # def __init__(self):
-  #   cmd.Cmd.__init__(self)
+  def __init__(self):
+    cmd.Cmd.__init__(self)
+    self.intro = """
 
-  def do_start(self, line):
-    """Hello there!"""
-    print('Hello')
-    MoonDocks.state = 1
+Game start
 
-  def postcmd(self, stop, line):
-    # Following should read from the global var but python
-    # is a fucking asshole.
-    userPrompt = '> ' if MoonDocks.state == 0 else 'What would you like to do? '
-    MoonDocks.prompt = userPrompt
-    return stop
+    """
 
   def do_exit(self, line):
     return True

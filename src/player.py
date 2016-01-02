1322c1f46e1010cbae9d64f1d8d2984b32a40eee
JBraun <jonbraun.deskpc@gmail.com>
2016-01-02 05:22:25 -0500

    Game Class add and Player class modified

--- src/player.py
+++ src/player.py
@@ -1,38 +1,127 @@
 # Player class
 
+import random
+
 class Player(object):
     """ A MoonDocks Player """
-    AFFILIATION = ["Empire", "Alliance"]
-    SPECIES = ["Human", "Crilac", "Flian", "Cyborg", "Droid"]
+    AFFILIATIONS = ["Alorean Empire", "United Rebel Alliance"]
+    SPECIES = ["Alorean", "Crilac", "Flian", "Cyborg", "Droid"]
 
-    def __init__(self, name, affiliation, species):
-        self.name = name
-        self.affiliation = affiliation
-        self.species = species
+    def __init__(self):
+        self.name = ""
+        self.affiliation = ""
+        self.species = ""
         self.rank = 0
 
     def __str__(self):
         toStr = self.name + ":\nAffiliation: " + self.affiliation + "\nSpecies: " + self.species + "\nRank: " + str(self.rank)
         return toStr
 
-    def setName(value):
-        self.name = value
+    def setName(self):
+        usrName = input("\nWhat is your name?: ")
+        self.name = usrName
 
-    def setAffiliation(value):
-        self.affiliation = value
+    def setAffiliation(self):
+        aff = """
+        ---------------------------------------------------------------------
+        | Affiliations:
+        |
+        | 1) {0}
+        | 2) {1}
+        ---------------------------------------------------------------------
+        """.format(self.AFFILIATIONS[0], self.AFFILIATIONS[1])
+        print(aff)
+        choice = input("Where do your loyalties lie? Enter Number: ")
+        self.affiliation = self.AFFILIATIONS[int(choice) - 1]
 
-def display_instruct():
-    """Display Game Instructions"""
-    print(
-    """
-        ----------------------------------------------------------------------
-        |MoonDocks: Fall of Illusions.                                       |
-        |The journey of Revolutionaries who don't know their true importance.|
-        |Join them in bringing freedom across the known galaxy.              |
-        ----------------------------------------------------------------------
-    """
-    )
-    input("To begin Enter 'begin journey' in the console.")
+    def setSpecies(self):
+        if self.affiliation == self.AFFILIATIONS[0]:
+            spec = """
+        ---------------------------------------------------------------------
+        | Species of the Alorean Empire:
+        |
+        | 1) {0} (Human species)
+        | 2) {1} (Reptilian species)
+        | 3) {2} (Insectoid slave species)
+        | 4) {3} (Reanimated empiric cyborgs)
+        | 5) {4} (Mastered AI offspring of the cyborgs)
+        | 6) I believe in fate, have destiny choose.
+        ---------------------------------------------------------------------
+            """.format(self.SPECIES[0], self.SPECIES[1], self.SPECIES[2], self.SPECIES[3], self.SPECIES[4])
+            print(spec)
+            choice = input("What Species where you born into? Enter Number: ")
+            if int(choice) < 6:
+                self.species = self.SPECIES[int(choice)-1]
+            else:
+                n = random.randrange(5)
+                if n == 0:
+                    self.species = self.SPECIES[int(n)]
+                else:
+                    self.species = self.SPECIES[int(n)-1]
+        else:
+            spec = """
+        ---------------------------------------------------------------------
+        | Species of the United Rebel Alliance:
+        |
+        | 1) {} (Human species)
+        | 2) {} (Insectoid slave species)
+        | 3) {} (Reanimated empiric cyborgs)
+        | 4) I believe in fate, have destiny choose.
+        ---------------------------------------------------------------------
+            """.format(self.SPECIES[0], self.SPECIES[2], self.SPECIES[3])
+            print(spec)
+            choice = input("What Species where you born into? Enter Number: ")
+            if int(choice) == 1:
+                self.species = self.SPECIES[int(choice)-1]
+            elif int(choice) <= 3:
+                self.species = self.SPECIES[int(choice)]
+            else:
+                n = random.randrange(3)
+                if int(n) == 1:
+                    self.species = self.SPECIES[int(n)-1]
+                elif int(n) <= 3:
+                    self.species = self.SPECIES[int(n)]
+
+class Game(object):
+    def display_instruct(self):
+        """Display Game Instructions"""
+        main = True
+
+        splash = """
+            ----------------------------------------------------------------------
+            |MoonDocks: Fall of Illusions.                                       |
+            |The journey of Revolutionaries who don't know their true importance.|
+            |Join them in bringing freedom across the known galaxy.              |
+            ----------------------------------------------------------------------
+        """
+        menu = """
+            ----------------------------------------------------------------------
+            | MAIN MENU:                                                         |
+            | ---------                                                          |
+            |                                                                    |
+            | 1) Start New Game.                                                 |
+            | 2) Load Game.                                                      |
+            | 3) Read Help.                                                      |
+            | 4) Quit.                                                           |
+            ----------------------------------------------------------------------
+        """
+        print(splash)
+        while main:
+            print(menu)
+            choice = int(input("What would you like to do?: "))
+            if choice == 1:
+                player = Player()
+                player.setName()
+                player.setAffiliation()
+                player.setSpecies()
+                print(player.__str__())
+                main = False
+            elif choice == 2:
+                print("Sorry, no players saved.")
+            elif choice == 3:
+                print("Help coming soon.")
+            elif choice == 4:
+                quit()
 
 def createPlayer():
     print("\t\tWelcome to Create Player")
@@ -43,8 +132,6 @@ def createPlayer():
     final_player = Player(name, affiliation, species)
     print(final_player.__str__() + " Created.")
 
-#display_instruct()
-#createPlayer()
-player = Player("Jon", "A", "human")
-print(player.__str__())
+game = Game()
+game.display_instruct()
 input("\n\nPress Enter to exit")
